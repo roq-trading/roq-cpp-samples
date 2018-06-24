5a8e8af5b92ad5cf837d80775a2f4cf36af02625
thraneh <thraneh@gmail.com>
2018-06-24 04:10:16 +0000

    optimizing stream/print operations

---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+/* Copyright (c) 2017-2018, Hans Erik Thrane */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
