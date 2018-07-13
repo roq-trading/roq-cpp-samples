3f985d566108858b08a21dfd5def115ec5eae9b3
thraneh <thraneh@gmail.com>
2018-07-13 14:06:00 +0000

    fix test directory (to compile and link)

--- test/test_time_schedule.cpp
+++ test/test_time_schedule.cpp
@@ -7,5 +7,8 @@
 using namespace examples::utilities;  // NOLINT
 
 TEST(timer, simple) {
-  Timer timer("Asia/Shanghai", { "12:00", "13:00", "14:00" }, "%H:%M");
+  TimeSchedule time_schedule(
+      "Asia/Shanghai",
+      { "12:00", "13:00", "14:00" },
+      "%H:%M");
 }
