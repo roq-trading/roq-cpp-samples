f4d3042380a1196a1a2a7b0ac1105238576a865d
thraneh <thraneh@gmail.com>
2019-05-29 05:28:47 +0000

    enable doxygen

--- conda/build.sh
+++ conda/build.sh
@@ -9,7 +9,7 @@ export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 
 ./autogen.sh
-./configure --prefix=$PREFIX --enable-benchmark
+./configure --prefix=$PREFIX --enable-benchmark --enable-doc
 make -j${CPU_COUNT:-2}
 set -e
 make check || EXIT_CODE=$?
