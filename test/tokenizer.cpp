852a2dda08e6d66e5082d41c32af7a71f00556e9
thraneh <thraneh@gmail.com>
2019-05-31 04:51:44 +0000

    fix warning

--- test/tokenizer.cpp
+++ test/tokenizer.cpp
@@ -8,40 +8,40 @@ using namespace roq::samples::common;  // NOLINT
 
 TEST(tokenizer, empty) {
   auto result = tokenize("", ",");
-  EXPECT_EQ(std::size(result), 0);
+  EXPECT_EQ(std::size(result), size_t {0});
 }
 
 TEST(tokenizer, single) {
   auto result = tokenize("abc123", ",");
-  ASSERT_EQ(std::size(result), 1);
-  ASSERT_EQ(std::size(result[0]), 6);
+  ASSERT_EQ(std::size(result), size_t {1});
+  ASSERT_EQ(std::size(result[0]), size_t {6});
   EXPECT_EQ(result[0], "abc123");
 }
 
 TEST(tokenizer, trivial) {
   auto result = tokenize("abc123,def", ",");
-  ASSERT_EQ(std::size(result), 2);
-  ASSERT_EQ(std::size(result[0]), 6);
+  ASSERT_EQ(std::size(result), size_t {2});
+  ASSERT_EQ(std::size(result[0]), size_t {6});
   EXPECT_EQ(result[0], "abc123");
-  ASSERT_EQ(std::size(result[1]), 3);
+  ASSERT_EQ(std::size(result[1]), size_t {3});
   EXPECT_EQ(result[1], "def");
 }
 
 TEST(tokenizer, empty_tokens) {
   auto result = tokenize(",", ",");
-  ASSERT_EQ(std::size(result), 2);
-  EXPECT_EQ(std::size(result[0]), 0);
-  EXPECT_EQ(std::size(result[1]), 0);
+  ASSERT_EQ(std::size(result), size_t {2});
+  EXPECT_EQ(std::size(result[0]), size_t {0});
+  EXPECT_EQ(std::size(result[1]), size_t {0});
 }
 
 TEST(tokenizer, some_empty_tokens) {
   auto result = tokenize("abc,,def,,", ",");
-  ASSERT_EQ(std::size(result), 5);
-  ASSERT_GT(std::size(result[0]), 0);
+  ASSERT_EQ(std::size(result), size_t {5});
+  ASSERT_GT(std::size(result[0]), size_t {0});
   EXPECT_EQ(result[0], "abc");
-  EXPECT_EQ(std::size(result[1]), 0);
-  ASSERT_GT(std::size(result[2]), 0);
+  EXPECT_EQ(std::size(result[1]), size_t {0});
+  ASSERT_GT(std::size(result[2]), size_t {0});
   EXPECT_EQ(result[2], "def");
-  EXPECT_EQ(std::size(result[3]), 0);
-  EXPECT_EQ(std::size(result[4]), 0);
+  EXPECT_EQ(std::size(result[3]), size_t {0});
+  EXPECT_EQ(std::size(result[4]), size_t {0});
 }
