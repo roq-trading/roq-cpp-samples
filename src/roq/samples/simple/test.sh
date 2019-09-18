06c5798a0c31a7c5097fb9b736423edefbc1796e
thraneh <thraneh@gmail.com>
2019-09-18 08:37:45 +0000

    script for testing deribit

---
+++ src/roq/samples/simple/test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+if [ "$1" == "debug" ]; then
+  PREFIX="libtool --mode=execute gdb --args"
+  shift 1
+else
+  PREFIX=
+fi
+
+NAME="trader"
+
+EXCHANGE="DERIBIT"
+ACCOUNTS="A1"
+SYMBOLS="BTC-27SEP19"
+
+$PREFIX "$DIR/roq-samples-simple" \
+	--name "$NAME" \
+	--dispatcher-affinity 0 \
+	--metrics "$(realpath "$DIR/${NAME}_metrics.sock")" \
+	--exchange "$EXCHANGE" \
+	--accounts "$ACCOUNTS" \
+	--symbols "$SYMBOLS" \
+	$@
