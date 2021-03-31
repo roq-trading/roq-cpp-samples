c13c480c431db37519f949e1e12adb88545d04dd
thraneh <thraneh@gmail.com>
2021-03-31 15:28:57 +0200

    cleanup

--- src/roq/samples/example-5/strategy.cpp
+++ src/roq/samples/example-5/strategy.cpp
@@ -13,92 +13,24 @@ namespace example_5 {
 Strategy::Strategy(client::Dispatcher &dispatcher) : dispatcher_(dispatcher) {
 }
 
-void Strategy::operator()(const Event<Connection> &event) {
+void Strategy::operator()(const Event<Start> &) {
+  // start thread
+}
+
+void Strategy::operator()(const Event<Stop> &) {
+  // join thread
+}
+
+void Strategy::operator()(const Event<TopOfBook> &event) {
   log::info(
-      "[{}:{}] Connection={}"_fmt,
+      "[{}:{}] TopOfBook={}"_fmt,
       event.message_info.source,
       event.message_info.source_name,
       event.value);
 }
 
-void Strategy::operator()(const Event<DownloadBegin> &event) {
+void Strategy::operator()(const Event<client::CustomMessage> &event) {
   log::info(
-      "[{}:{}] DownloadBegin={}"_fmt,
-      event.message_info.source,
-      event.message_info.source_name,
-      event.value);
-}
-
-void Strategy::operator()(const Event<DownloadEnd> &event) {
-  log::info(
-      "[{}:{}] DownloadEnd={}"_fmt,
-      event.message_info.source,
-      event.message_info.source_name,
-      event.value);
-}
-
-void Strategy::operator()(const Event<GatewaySettings> &event) {
-  log::info(
-      "[{}:{}] Settings={}"_fmt,
-      event.message_info.source,
-      event.message_info.source_name,
-      event.value);
-}
-
-void Strategy::operator()(const Event<StreamUpdate> &event) {
-  log::info(
-      "[{}:{}] StreamUpdate={}"_fmt,
-      event.message_info.source,
-      event.message_info.source_name,
-      event.value);
-}
-
-void Strategy::operator()(const Event<ExternalLatency> &event) {
-  // only verbose logging, see comment in header
-  log::trace_1(
-      "[{}:{}] ExternalLatency={}"_fmt,
-      event.message_info.source,
-      event.message_info.source_name,
-      event.value);
-}
-
-void Strategy::operator()(const Event<ReferenceData> &event) {
-  log::info(
-      "[{}:{}] ReferenceData={}"_fmt,
-      event.message_info.source,
-      event.message_info.source_name,
-      event.value);
-}
-
-void Strategy::operator()(const Event<MarketStatus> &event) {
-  log::info(
-      "[{}:{}] MarketStatus={}"_fmt,
-      event.message_info.source,
-      event.message_info.source_name,
-      event.value);
-}
-
-void Strategy::operator()(const Event<MarketByPriceUpdate> &event) {
-  // only verbose logging, see comment in header
-  log::trace_1(
-      "[{}:{}] MarketByPriceUpdate={}"_fmt,
-      event.message_info.source,
-      event.message_info.source_name,
-      event.value);
-}
-
-void Strategy::operator()(const Event<MarketByOrderUpdate> &event) {
-  // only verbose logging, see comment in header
-  log::trace_1(
-      "[{}:{}] MarketByOrderUpdate={}"_fmt,
-      event.message_info.source,
-      event.message_info.source_name,
-      event.value);
-}
-
-void Strategy::operator()(const Event<TradeSummary> &event) {
-  // only verbose logging, see comment in header
-  log::trace_1(
       "[{}:{}] TradeSummary={}"_fmt,
       event.message_info.source,
       event.message_info.source_name,
--- src/roq/samples/example-5/strategy.h
+++ src/roq/samples/example-5/strategy.h
@@ -9,14 +9,6 @@ namespace roq {
 namespace samples {
 namespace example_5 {
 
-// client::Handler is the base class managing the life-time of
-// a strategy (could be in-process simulation or connectivity
-// to one or more market access gateways)
-// the constructor must *always* accept a client::Dispatcher as the
-// first argument
-// the construct *may* accept more arguments from the 2nd position
-// event handlers *may* be overriden
-
 class Strategy final : public client::Handler {
  public:
   explicit Strategy(client::Dispatcher &);
@@ -25,26 +17,10 @@ class Strategy final : public client::Handler {
   Strategy(const Strategy &) = delete;
 
  protected:
-  // the following event handlers log every update as-is
-  // an important lesson here is to only use only verbose logging in
-  // the "hot path" (i.e. inside the event handlers)
-  // using log::info(...) is not disallowed, but you should only use
-  // this logging facility very minimally as it may have undesirable
-  // latency impact
-  // note!
-  //   the ROQ_v environment variable controls the verbosity level
-  //   for example, "export ROQ_v=1"
-  void operator()(const Event<Connection> &) override;
-  void operator()(const Event<DownloadBegin> &) override;
-  void operator()(const Event<DownloadEnd> &) override;
-  void operator()(const Event<GatewaySettings> &) override;
-  void operator()(const Event<StreamUpdate> &) override;
-  void operator()(const Event<ExternalLatency> &) override;
-  void operator()(const Event<ReferenceData> &) override;
-  void operator()(const Event<MarketStatus> &) override;
-  void operator()(const Event<MarketByPriceUpdate> &) override;
-  void operator()(const Event<MarketByOrderUpdate> &) override;
-  void operator()(const Event<TradeSummary> &) override;
+  void operator()(const Event<Start> &) override;
+  void operator()(const Event<Stop> &) override;
+  void operator()(const Event<TopOfBook> &) override;
+  void operator()(const Event<client::CustomMessage> &) override;
 
  private:
   client::Dispatcher &dispatcher_;
