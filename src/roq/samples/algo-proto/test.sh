e668ca7fbdc82b10b266fcaed276a1cca9bc5320
thraneh <thraneh@gmail.com>
2022-03-21 08:27:38 +0100

    add launch script + config toml file

---
+++ src/roq/samples/algo-proto/test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+CONFIG_FILE="config.toml"
+
+# debug?
+
+if [ "$1" == "debug" ]; then
+  KERNEL="$(uname -a)"
+  case "$KERNEL" in
+    Linux*)
+      PREFIX="gdb --args"
+      ;;
+    Darwin*)
+      PREFIX="lldb --"
+      ;;
+  esac
+  shift 1
+else
+	PREFIX=
+fi
+
+# launch
+
+$PREFIX "./roq-samples-algo-proto" \
+	--name "trader" \
+	--config_file "$CONFIG_FILE" \
+  --strategy "spread_btc" \
+  --side BUY \
+  --quantity 1 \
+  --price 100 \
+	$@
