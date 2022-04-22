87caad4f44ba9221b97cfaa8f14bc36537ede0d6
thraneh <thraneh@gmail.com>
2022-04-22 21:56:25 +0200

    chore

--- src/roq/samples/zeromq/zmq/context.cpp
+++ src/roq/samples/zeromq/zmq/context.cpp
@@ -2,6 +2,8 @@
 
 #include "roq/samples/zeromq/zmq/context.hpp"
 
+#include <cassert>
+
 #include "roq/exceptions.hpp"
 
 using namespace std::literals;
@@ -38,13 +40,15 @@ Context::Context() : handle_(create<decltype(handle_)>()) {
 void Context::set(int option_name, int option_value) {
   auto result = zmq_ctx_set(handle_.get(), option_name, option_value);
   if (result == -1)
-    throw RuntimeError{"zmq_ctx_set"sv};
+    throw SystemError{std::make_error_code(static_cast<std::errc>(errno)), "zmq_ctx_set"sv};
+  assert(result == 0);
 }
 
 int Context::get(int option_name) {
   auto result = zmq_ctx_get(handle_.get(), option_name);
   if (result == -1)
-    throw RuntimeError{"zmq_ctx_get"sv};
+    throw SystemError{std::make_error_code(static_cast<std::errc>(errno)), "zmq_ctx_get"sv};
+  assert(result >= 0);
   return result;
 }
 
